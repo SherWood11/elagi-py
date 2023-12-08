e583148d148cf2124ce876bc164a847c81f632d1
SherWood11 <103008333+SherWood11@users.noreply.github.com>
2023-12-08 16:04:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function submitForm() {
     // You can add JavaScript logic to handle form submission here
-    alert("Form submitted successfully!");
+    alert("Make sure all information is correct and click \"OK\"!"");
   }
   
